c27d15599be50f9dbdd65b6de326c68d95a9dfc0
Eric Waring <ericw@the-feel.co.uk>
2015-12-13 13:30:24 +0000

    Update to flash.now

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -24,8 +24,8 @@ RSpec.feature 'Creating Articles' do
     click_button 'Create Article'
 
     expect(page).to have_content('Article has not been created')
-    expect(page).to have_content("Title can't be blank")
-    expect(page).to have_content("Body can't be blank")
+    expect(page).to have_content('Title must not be blank')
+    expect(page).to have_content('Body must not be blank')
 
   end
 
