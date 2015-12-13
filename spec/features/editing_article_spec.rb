749dd68ebe52dda457dfac21a0249060f120aee1
Eric Waring <ericw@the-feel.co.uk>
2015-12-13 23:30:49 +0000

    setup sign out for users

--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.feature 'Editing an Article' do
 
   before do
+    @john = User.create(email: 'john@example.com', password: 'password')
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Email', with: @john.email
+    fill_in 'Password', with: @john.password
+    click_button 'Log in'
     @article = Article.create(title: 'The first article', body: 'Body of the first article')
   end
 
@@ -21,17 +27,17 @@ RSpec.feature 'Editing an Article' do
   end
 
   scenario 'A user fails to update an article' do
-      visit '/'
+    visit '/'
 
-      click_link @article.title
-      click_link 'Edit Article'
+    click_link @article.title
+    click_link 'Edit Article'
 
-      fill_in 'Title', with: ''
-      fill_in 'Body', with: 'Updated body of article'
-      click_button('Update Article')
+    fill_in 'Title', with: ''
+    fill_in 'Body', with: 'Updated body of article'
+    click_button('Update Article')
 
-      expect(page).to have_content('Article has not been updated')
-      expect(page.current_path).to eq(article_path(@article))
-    end
+    expect(page).to have_content('Article has not been updated')
+    expect(page.current_path).to eq(article_path(@article))
+  end
 
 end
