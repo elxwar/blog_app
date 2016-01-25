ba3a76d16fad4c5da17b7ade22250ab76c961527
Eric Waring <ericw@the-feel.co.uk>
2016-01-25 07:58:11 +0000

    implement adding comments

---
+++ spec/features/adding_comments_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'Adding reviews to articles' do
+
+  before do
+    @john = User.create(email: 'john@example.com', password: 'password')
+    @fred = User.create(email: 'fred@example.com', password: 'password')
+
+    @article = Article.create(title: 'The first article', body: 'Body of the first article', user: @john)
+  end
+  scenario 'Permits a signed in user to add a review' do
+    login_as @fred
+    visit '/'
+
+    click_link @article.title
+    fill_in 'New Comment', with: 'A wicked article'
+    click_button 'Add Comment'
+
+    expect(page).to have_content('Comment has been created')
+    expect(page).to have_content('A wicked article')
+    expect(current_path).to eq(article_path(@article.comments.last.id))
+  end
+end
