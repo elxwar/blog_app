a068364c4c0edd9c4930eb7042527220d7ba44f7
Eric Waring <ericw@the-feel.co.uk>
2016-01-26 09:28:38 +0000

    Force sign in or up before posting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 class CommentsController < ApplicationController
   before_action :set_article
   def create
+    unless current_user
+      flash[:danger] = 'Please sign in or sign up first'
+      redirect_to new_user_session_path
+    else
     @comment = @article.comments.build(comment_params)
     @comment.user = current_user
 
@@ -11,7 +15,7 @@ class CommentsController < ApplicationController
     end
     redirect_to article_path(@article)
   end
-
+end
   private
 
   def comment_params
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
+require 'support/macros'
 
 RSpec.describe CommentsController, type: :controller do
+  describe 'POST #create' do
+    before do
+      @john = User.create(email: 'john@example.com', password: 'password')
+    end
 
+    context 'signed in user' do
+      it 'can create a comment' do
+        sign_in @john
+        article = Article.create(title: 'First Article',
+          body: 'Body of 1st article',
+          user: @john)
+        post :create, comment: {body: 'Awesome post'},
+          article_id: article.id
+        expect(flash[:success]).to eq('Comment has been created')
+      end
+    end
+
+    context 'non-signed in user' do
+      it 'is redirected to the sign in page' do
+        sign_in ''
+        article = Article.create(title: 'First Article',
+          body: 'Body of 1st article',
+          user: @john)
+        post :create, comment: {body: 'Awesome post'},
+          article_id: article.id
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
 end
